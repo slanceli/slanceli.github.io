986f9dfc90476823222c237d5ef6f56a61e54214
slanceli <ExcitedOOXX@gmail.com>
2019-02-15 13:39:15 +0800

    Site updated: 2019-02-15 13:39:08

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon.ico");
          document.title = '≥＾≤ 我藏起来啦 ~';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
+         $('[rel="icon"]').attr('href', "/images/favicon.ico");
          document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
